27f21ff6f791213034df1e5e362504cee7106bd4
dyaom <dyaomaster@yahoo.com>
2019-04-16 09:12:11 -0700

    Added methods to Paddle class

--- src/Pong/Paddle.java
+++ src/Pong/Paddle.java
@@ -3,47 +3,74 @@ package Pong;
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
-
 import java.awt.Color;
 import java.awt.Graphics;
 
-public class Paddle extends Block
-{
-   //instance variables
-   private int speed;
+public class Paddle extends Block {
 
-   public Paddle()
-   {
-		super(10,10);
-      speed =5;
-   }
+    //instance variables
+    private int speed;
 
+    public Paddle() {
+        super(10, 10);
+        speed = 5;
+    }
 
-   //add the other Paddle constructors
+    public Paddle(int x, int y) {
+        super(x, y);
+        speed = 5;
+    }
 
+    public Paddle(int x, int y, int vy) {
+        super(x, y);
+        speed = vy;
+    }
 
+    public Paddle(int x, int y, int width, int height) {
+        super(x, y, width, height);
+        speed = 5;
+    }
 
+    public Paddle(int x, int y, int width, int height, int vy) {
+        super(x, y, width, height);
+        speed = vy;
+    }
 
+    public Paddle(int x, int y, int width, int height, Color color, int vy) {
+        super(x, y, width, height, color);
+        speed = vy;
+    }
 
+    public void moveUpAndDraw(Graphics window) {
+        window.setColor(Color.WHITE);
+        window.fillRect(getX(), getY(), getWidth(), getHeight());
 
+        setY(getY() - getSpeed());
 
+        window.setColor(getColor());
+        window.fillRect(getX(), getY(), getWidth(), getHeight());
+    }
 
+    public void moveDownAndDraw(Graphics window) {
+        window.setColor(Color.WHITE);
+        window.fillRect(getX(), getY(), getWidth(), getHeight());
 
+        setY(getY() + getSpeed());
 
-   public void moveUpAndDraw(Graphics window)
-   {
+        window.setColor(getColor());
+        window.fillRect(getX(), getY(), getWidth(), getHeight());
 
-
-   }
-
-   public void moveDownAndDraw(Graphics window)
-   {
-
-
-   }
+    }
 
    //add get methods
-   
-   
-   //add a toString() method
+    public int getSpeed() {
+        return speed;
+    }
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    
+    public String toString() {
+        return super.toString() + " " + speed;
+    }
 }
