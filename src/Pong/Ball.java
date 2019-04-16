0cd0794d476235493974b1ef756f9fab680ead03
dyaom <dyaomaster@yahoo.com>
2019-04-16 08:29:51 -0700

    Added methods to Ball class

--- src/Pong/Ball.java
+++ src/Pong/Ball.java
@@ -16,23 +16,65 @@ public class Ball extends Block {
         xSpeed = 3;
         ySpeed = 1;
     }
+    public Ball(int x, int y) {
+        super(x, y);
+        xSpeed = 3;
+        ySpeed = 1;
+    }
+    public Ball(int x, int y, int width, int height) {
+        super(x, y, width, height);
+        xSpeed = 3;
+        ySpeed = 1;
+    }
+    
+    public Ball(int x, int y, int width, int height, Color color) {
+        super(x, y, width, height, color);
+        xSpeed = 3;
+        ySpeed = 1;
+    }
+    
+    public Ball(int x, int y, int width, int height, Color color, int vx, int vy) {
+        super(x, y, width, height, color);
+        xSpeed = vx;
+        ySpeed = vy;
+    }
 
-	//add the other Ball constructors
    //add the set methods
+    public void setXSpeed(int vx) {
+        xSpeed = vx;
+    }
+    public void setYSpeed(int vy) {
+        ySpeed = vy;
+    }
+    
     public void moveAndDraw(Graphics window) {
    	//draw a white ball at old ball location
 
         setX(getX() + xSpeed);
 		//setY
+        setY(getY() + ySpeed);
 
         //draw the ball at its new location
     }
 
     public boolean equals(Object obj) {
-
-        return false;
+        Ball comp = (Ball)obj;
+        return (xSpeed == comp.getXSpeed() && ySpeed == comp.getYSpeed() &&
+                super.equals(comp));
     }
 
    //add the get methods
+    public int getXSpeed() {
+        return xSpeed;
+    }
+    public int getYSpeed() {
+        return ySpeed;
+    }
+    
    //add a toString() method
+    public String toString() {
+        return getX() + " " + getY() + " " + 
+                getWidth() + " " + getHeight() + " " + 
+                getColor() + " " + xSpeed + " " + ySpeed;
+    }
 }
