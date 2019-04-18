54b7f0d9a49d2b977acff675f9c48c7b0b51d32e
dyaom <dyaomaster@yahoo.com>
2019-04-18 08:21:04 -0700

    Implemented SpeedUpBall; decided not to use

--- src/Pong/SpeedUpBall.java
+++ src/Pong/SpeedUpBall.java
@@ -3,64 +3,43 @@ package Pong;
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
-
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.Random;
 
-class SpeedUpBall extends Ball
-{
+class SpeedUpBall extends Ball {
 
-   //instance variables
+    //instance variables
+    public SpeedUpBall() {
+        super();
+    }
 
-   public SpeedUpBall()
-   {
+    public SpeedUpBall(int x, int y) {
+        super(x, y);
+    }
 
+    public SpeedUpBall(int x, int y, int wid, int ht) {
+        super(x, y, wid, ht);
+    }
 
-   }
+    public SpeedUpBall(int x, int y, int wid, int ht, Color col) {
+        super(x, y, wid, ht, col);
+    }
 
-   public SpeedUpBall(int x, int y)
-   {
+    public SpeedUpBall(int x, int y, int wid, int ht, int xSpd, int ySpd) {
+        super(x, y, wid, ht, xSpd, ySpd);
+    }
 
+    public SpeedUpBall(int x, int y, int wid, int ht, Color col, int xSpd, int ySpd) {
+        super(x, y, wid, ht, col, xSpd, ySpd);
+    }
 
-   }
+    public void setXSpeed(int xSpd) {
+        super.setXSpeed((int)(Math.ceil(xSpd * 1.1)));
+    }
 
+    public void setYSpeed(int ySpd) {
+        super.setYSpeed(ySpd);
 
-   public SpeedUpBall(int x, int y, int xSpd, int ySpd)
-   {
-
-
-   }
-
-   public SpeedUpBall(int x, int y, int wid, int ht, int xSpd, int ySpd)
-   {
-
-
-   }
-
-
-   public SpeedUpBall(int x, int y, int wid, int ht, Color col, int xSpd, int ySpd)
-   {
-
-
-
-
-   }
-
-   public void setXSpeed( int xSpd )
-   {
-
-
-
-
-   }
-
-   public void setYSpeed( int ySpd )
-   {
-
-
-
-
-   }
+    }
 }
-
